d72b65d3fac5218ce926d82bc1c826882a9f4de0
kindai <kintomiko@gmail.com>
2015-02-06 03:31:45 +0800

    batch proxy

--- Loginer.py
+++ Loginer.py
@@ -2,21 +2,25 @@ import urllib, urllib2, cookielib, json
 
 class Session(object):
 
-	def __init__(self):
+	def __init__(self, proxy=None, debugLevel=0):
 		self.cj = cookielib.CookieJar()
 		cookies = urllib2.HTTPCookieProcessor(self.cj)
-		httpHandler = urllib2.HTTPHandler(debuglevel=1)
-		self.opener = urllib2.build_opener(cookies, httpHandler)
+		httpHandler = urllib2.HTTPHandler(debuglevel=debugLevel)
+		if proxy!=None:
+			proxyHandler = urllib2.ProxyHandler({'http': 'http://%s/' % proxy})
+			self.opener = urllib2.build_opener(cookies, httpHandler, proxyHandler)
+		else:
+			self.opener = urllib2.build_opener(cookies, httpHandler)
 		urllib2.install_opener(self.opener)
 		self.opener.addheaders.append(('Accept-Language', 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,ja;q=0.2,zh-TW;q=0.2'))
 		self.opener.addheaders.append(('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36'))
 		self.opener.addheaders.append(('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'))
 		self.opener.addheaders.append(('Connection', 'Keep-Alive'))
 
-	def open(self, url, data=None):
+	def open(self, url, data=None, time_out=20):
 		if data == None:
-			return self.opener.open(url)
-		return self.opener.open(url, data)
+			return self.opener.open(url, timeout=time_out)
+		return self.opener.open(url, data, timeout=time_out)
 
 	def cookie(self):
 		rst=''
